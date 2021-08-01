3f448784f29f0a4443b0f269a44c1db16b99733b
Cadu Barbosa <69665244+barbosstech@users.noreply.github.com>
2021-08-01 20:49:28 -0300

    Update Clonar.bat

--- Clonar.bat
+++ Clonar.bat
@@ -6,6 +6,7 @@ CLS
 @ECHO ...............................................
 @ECHO Sistema de Clone Windows 10 2H21
 @ECHO Por Carlos Eduardo Barbosa Jul/21
+@ECHO MANTENHA O COMPUTADOR CONECTADO NA FONTE!
 @ECHO ...............................................
 @ECHO.
 @ECHO Versao da imagem desejada:
@@ -13,7 +14,7 @@ CLS
 @ECHO 1 - Dell Latitude 3410
 @ECHO 2 - Dell Latitude 7000
 @ECHO 3 - Windows 10 Generico (Sysprep)
-@ECHO 4 - Windows 10 - Pre Linux
+@ECHO 4 - Windows 10 - Pre Linux (Sysprep)
 @ECHO 5 - $ Reservado $
 @ECHO 6 - $ Reservado $
 @ECHO.
@@ -25,14 +26,16 @@ IF %M%==4 GOTO SDA
 IF %M%==5 GOTO EXIT
 IF %M%==6 GOTO EXIT
 :3410
-D:\ApplyImage.bat E:\Imagens\PRIOLatitude3410.wim
+D:\ApplyImage.bat
 GOTO MENU
 :7000
-D:\ApplyImage1.bat E:\Imagens\PRIOLatitude3410.wim
+D:\ApplyImage1.bat
 GOTO MENU
 :GEN
-D:\ApplyImageGeneric.bat E:\Imagens\PRIOSysPrep.wim
+D:\ApplyImageGeneric.bat
+GOTO MENU
 :SDA
-D:\ApplyImageOnLinux.bat C:\Imagens\PRIOSysPrep.wim
+D:\ApplyImageOnLinux.bat
+GOTO MENU
 :EXIT
 GOTO MENU
